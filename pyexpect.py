0a794792b2c0ff622d04275b3d3a60186e59547f
Vikrant Sagar <sagar.vikrant@gmail.com>
2013-04-20 19:07:13 -0400

    pattern engine halfway

--- pyexpect.py
+++ pyexpect.py
@@ -2,8 +2,9 @@ import sys
 import re
 import json
 import marisa_trie
+import pyannotate
 
-DEBUG = 1
+DEBUG = 0
 
 #read the json file
 def readJson():
@@ -12,8 +13,32 @@ def readJson():
 	expect = expect_json["expectations"]
 	return expect
 
-#main function
-def main():
+#get start and end index of a regex pattern
+def getIndices(sent,pattern):
+	m = re.search(pattern,sent)
+	if m:
+		return m.start(),m.end()
+	else:
+		return -1,-1
+
+
+def extract(sent,patterns):
+	if sent == "":
+		return 
+	else:
+		for pat in patterns:
+			start,end = getIndices(sent,pat)
+			if start > -1:
+				extract(sent[:start],)
+
+#init function
+def extract_init(sent,classes):
+	persons = pyannotate.toLowerCase(classes[0])
+	books = pyannotate.toLowerCase(classes[0])
+
+	ptrie = marisa_trie.Trie(persons)
+	btrie = marisa_trie.Trie(books)
+
 	exp = readJson()
 	if DEBUG:
 		print "json object = ",exp
@@ -27,4 +52,18 @@ def main():
 
 
 if __name__ == '__main__':	
-	main()
+	sent = "Charles Dickens wrote books like A Christmas Carol, Anthony and Mayan, A Chritmas Carol"
+	persons = [u"Charles Dickens"]
+	books = [u"A Christmas Carol",u"Anthony",u"Mayan"]
+
+	#test annotation
+	ptrie = marisa_trie.Trie(pyannotate.toLowerCase(persons))
+	btrie = marisa_trie.Trie(pyannotate.toLowerCase(books))
+	#pyannotate.annotate(sent,ptrie)
+
+	#test pattern indices
+	start,end = getIndices(sent,"[C|c]arol")
+	print start, end
+
+	#main
+	extract_init(sent,[persons,books])
