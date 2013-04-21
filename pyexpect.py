58660e712cadb804a6a4a50a12c5f97dbac32c64
Vikrant Sagar <sagar.vikrant@gmail.com>
2013-04-20 20:49:22 -0400

    pattern engine mostly done

--- pyexpect.py
+++ pyexpect.py
@@ -21,15 +21,41 @@ def getIndices(sent,pattern):
 	else:
 		return -1,-1
 
+def getLeftPatterns(patterns,i):
+	return patterns[:i]
 
-def extract(sent,patterns):
-	if sent == "":
-		return 
+def getRightPatterns(patterns,i):
+	return patterns[i+1:]
+
+def partition(sent,patterns):
+	if sent == "" or len(patterns) == 0:
+		if len(patterns) == 0:
+			return [sent]
+		else:
+			return [""]
 	else:
-		for pat in patterns:
-			start,end = getIndices(sent,pat)
-			if start > -1:
-				extract(sent[:start],)
+		regexPattern = 0
+		for i in range(len(patterns)):
+			if not (patterns[i][1] in ["$","@"]):
+				#print "pat",patterns[i]
+				regexPattern = 1
+				l = len(patterns[i].strip()) - 1 
+				pat = patterns[i].strip()
+				pat = pat[1:l]
+
+				print pat
+
+				start,end = getIndices(sent,pat)
+				if start > -1:
+					#print sent[start:end+1]
+					left  = partition(sent[:start],getLeftPatterns(patterns,i))
+					right = partition(sent[end+1:],getRightPatterns(patterns,i))
+					return left + ["@"+pat] + right
+				else:
+					return [""]
+
+		if regexPattern == 0:
+			return [sent]
 
 #init function
 def extract_init(sent,classes):
@@ -44,15 +70,22 @@ def extract_init(sent,classes):
 		print "json object = ",exp
 
 	for exp_one in exp:
+
 		if DEBUG:
 			print "first element = ",exp_one
+
 		patterns = exp_one["patterns"]
-		if DEBUG:
+
+		if DEBUG == 0:
 			print "patterns = ",patterns
 
+		for pattern in patterns:
+			pat = pattern.split(",")
+			print partition(sent,pat)
 
 if __name__ == '__main__':	
 	sent = "Charles Dickens wrote books like A Christmas Carol, Anthony and Mayan, A Chritmas Carol"
+	sent1 = "hello from book me to kill me"
 	persons = [u"Charles Dickens"]
 	books = [u"A Christmas Carol",u"Anthony",u"Mayan"]
 
@@ -63,7 +96,7 @@ if __name__ == '__main__':
 
 	#test pattern indices
 	start,end = getIndices(sent,"[C|c]arol")
-	print start, end
+	#print start, end
 
 	#main
-	extract_init(sent,[persons,books])
+	extract_init(sent1,[persons,books])
